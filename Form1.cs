e10427f8e66d1e491aea69ad080040e9f75e7d51
liamGman <73509580+liamGman@users.noreply.github.com>
2021-02-02 17:09:50 -0600

    Update Form1.cs
    There could be errors in here,

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,8 @@ namespace GreenvilleRevenueGUI
 {
     public partial class Form1 : Form
     {
+        double const FEE = 25.00;
+        
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,15 @@ namespace GreenvilleRevenueGUI
 
         private void calcRevenueBtn_Click(object sender, EventArgs e)
         {
-
+            contestents1 = inputLastYeat.Text;
+            contestents2 = inputThisYeat.Text;
+            double revenue = int.Parse(contestents2) * FEE;
+            outputRevenue.Text = revenue.ToString("C");
+            outputRevenue.Visible = true;
+            bool yearTwoIsBigger = double.Parse(contestents2) > double.Parse(contestents1);
+            outputMoreOrLess.Text = String.Format("It is {0} that this year has more contestents than last year.", yearTwoIsBigger);
+            outputMoreOrLess.Visible = true;
+            
         }
     }
 }
